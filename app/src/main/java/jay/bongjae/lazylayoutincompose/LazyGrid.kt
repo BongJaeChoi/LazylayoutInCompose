b6900db46d9bc6149b1bf6c076cd7225f2f288f0
BongJaeChoi <jjgod0124@gmail.com>
2023-01-12 20:51:43 +0900

    LazyGrid Span 적용

--- app/src/main/java/jay/bongjae/lazylayoutincompose/LazyGrid.kt
+++ app/src/main/java/jay/bongjae/lazylayoutincompose/LazyGrid.kt
@@ -1,13 +1,14 @@
 package jay.bongjae.lazylayoutincompose
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.grid.itemsIndexed
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -17,11 +18,19 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun LazyVerticalGridPractice() {
+    val state = rememberLazyGridState()
     LazyVerticalGrid(
-        columns = GridCells.Fixed(2),
-        verticalArrangement = Arrangement.spacedBy(16.dp),
-        horizontalArrangement = Arrangement.spacedBy(16.dp)
+        columns = GridCells.Adaptive(64.dp),
+        state = state,
     ) {
+        item(span = {
+            GridItemSpan(maxLineSpan)
+        }) {
+            Text(text = "제목", modifier = Modifier
+                .fillMaxWidth()
+                .heightIn(16.dp))
+        }
+
         itemsIndexed(dummy) { index, item ->
             Column(
                 Modifier
