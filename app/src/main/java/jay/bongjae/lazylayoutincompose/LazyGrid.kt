a808ff461142d95d8e83476eac2c2fdd840cbb76
BongJaeChoi <jjgod0124@gmail.com>
2023-01-12 20:44:25 +0900

    LazyGrid 샘플 구현 연습

---
+++ app/src/main/java/jay/bongjae/lazylayoutincompose/LazyGrid.kt
@@ -0,0 +1,34 @@
+package jay.bongjae.lazylayoutincompose
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.grid.itemsIndexed
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun LazyVerticalGridPractice() {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        itemsIndexed(dummy) { index, item ->
+            Column(
+                Modifier
+                    .background(Color.LightGray)
+            ) {
+                Text(text = "name is ${item.name} / index : $index")
+            }
+        }
+    }
+}
